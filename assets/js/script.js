562d8089b5e9fda2136bc1140594fdef2677a505
bryance97 <bryancifuentesescobar@gmail.com>
2021-12-15 01:14:47 -0500

    display time and tweak notification calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
-    // listens save button
-    $('.saveBtn').on('click', function() {
-      // get back values
-      var value = $(this).siblings('.description').val().trim();
-      var time = $(this).parent().attr('id');
+var currentDate = document.getElementById("currentDay");
+var dateFormat = "dddd, MMMM Do";
+currentDate.textContent = moment().format(dateFormat);
 
-        //localstorage
-        localStorage.setItem(time, value);
-    })
+$(document).ready(function () {
+  // listens save button
+  $(".saveBtn").on("click", function () {
+    // get back values
+    var value = $(this).siblings(".description").val().trim();
+    var time = $(this).parent().attr("id");
+
+    //localstorage
+    localStorage.setItem(time, value);
+  });
+
+  $(".notification").addClass("show");
+
+  setTimeout(function () {
+    $(".notification").removeClass("show");
+  }, 5000);
 });
