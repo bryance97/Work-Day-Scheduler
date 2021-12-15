4d52d187ed190c597f200da26e1c25a4cf53da21
bryance97 <bryancifuentesescobar@gmail.com>
2021-12-15 12:59:20 -0500

    adding js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var currentDate = document.getElementById("currentDay");
 var dateFormat = "dddd, MMMM Do";
 currentDate.textContent = moment().format(dateFormat);
 
+var currentHour = moment().hours();
+
+
 $(document).ready(function () {
   // listens save button
   $(".saveBtn").on("click", function () {
@@ -11,11 +14,41 @@ $(document).ready(function () {
 
     //localstorage
     localStorage.setItem(time, value);
+
+    hourUpdater(time);
+
   });
 
-  $(".notification").addClass("show");
 
-  setTimeout(function () {
-    $(".notification").removeClass("show");
-  }, 5000);
-});
+  //gets current time
+  function hourUpdater() {
+ 
+    //checcks and applies right color to current time
+    var hourUpdater = function (time) {
+      if (currentHour < time) {
+        $(this).addClass("future");
+      } else if (currentHour > time) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    };
+
+    hourUpdater();
+
+};
+    ///localstorage values returned
+    $("#nine-am .description").val(localStorage.getItem("nine-am"));
+    $("#ten-am .description").val(localStorage.getItem("ten-am"));
+    $("#eleven-am .description").val(localStorage.getItem("eleven-am"));
+    $("#twelve-pm .description").val(localStorage.getItem("twelve-pm"));
+    $("#one-pm .description").val(localStorage.getItem("one-pm"));
+    $("#two-pm .description").val(localStorage.getItem("two-pm"));
+    $("#three-pm .description").val(localStorage.getItem("three-pm"));
+    $("#four-pm .description").val(localStorage.getItem("four-pm"));
+    $("#five-pm .description").val(localStorage.getItem("five-pm"));
+
+})
